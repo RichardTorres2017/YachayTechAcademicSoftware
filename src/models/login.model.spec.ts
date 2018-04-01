d7cffffe15d70b4c93bab1de1dcbf113e133890f
RichardTorres <richard.torres@yachaytech.edu.ec>
2018-04-01 17:05:21 -0500

    Change message testing

--- src/models/login.model.spec.ts
+++ src/models/login.model.spec.ts
@@ -3,7 +3,7 @@ import {Login} from './login.model';
 describe("Test for login",()=>{
 
     describe("Test for username and password correct",()=>{
-        it("should return the username and password correct",()=>{
+        it(" The username and password correct",()=>{
             let login = new Login("richard.torres","richard123");
         expect(login.getLogin()).toEqual("richard.torres richard123");
         });
@@ -11,19 +11,19 @@ describe("Test for login",()=>{
 
     });
     describe("Test for username and password incorrect",()=>{
-        it("should return the username and password incorrect",()=>{
+        it("The username and password incorrect",()=>{
         let login = new Login("richard.torres","richard123");
         expect(login.getLogin()).not.toContain("richard.torres rihard123");
         });
     });
     describe("Test for username incorrect and password correct",()=>{
-        it("should return the  username incorrect and password correct",()=>{
+        it("The  username incorrect and password correct",()=>{
         let login = new Login("richard.torres","richard123");
         expect(login.getLogin()).not.toContain("richard.torr richard123");
     });
     });
     describe("Test for username incorrect and password incorrect",()=>{
-        it("should return the  username incorrect and password incorrect",()=>{
+        it("The  username incorrect and password incorrect",()=>{
         let login = new Login("richard.torres","richard123");
         expect(login.getLogin()).not.toContain("richard.tores ricard123");
     });

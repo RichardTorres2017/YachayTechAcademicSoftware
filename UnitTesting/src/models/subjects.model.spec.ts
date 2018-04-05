28b728c8d007e66777bf3185537411e43b755dd1
wjmorejon <winter.morejon@yachaytech.edu.ec>
2018-04-04 20:04:05 -0500

    Subjects spec modify

--- UnitTesting/src/models/subjects.model.spec.ts
+++ UnitTesting/src/models/subjects.model.spec.ts
@@ -12,7 +12,7 @@ describe("Test for subject",()=>{
       describe("Test for not connected",()=>{
         it("Should show an error that it does not have internet",()=>{
           let subjects = new Subjects ("","");
-          expect(subjects.getSubjects()).toEqual("");
+          expect(subjects.getSubjects()).toEqual(" ");
         });
       });
     });
